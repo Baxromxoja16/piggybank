68b5ef2056ec4881c02b0a44ed72d9ac72851242
Baxromxoja <baxrom.aglamkhujaev@payme.uz>
2023-12-13 17:37:44 +0500

    feat: added main page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { MainComponent } from './pages/main/main.component';
+import { authGuard } from './auth/guards/auth.guard';
 
 export const routes: Routes = [
   {
@@ -8,4 +10,5 @@ export const routes: Routes = [
     pathMatch: 'full',
   },
   { path: 'auth', component: LoginComponent },
+  { path: 'main', canActivate: [authGuard], component: MainComponent },
 ];
