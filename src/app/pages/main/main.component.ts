16dc287e6ff25a1b1337fc3e54dd8189a2148ef0
Baxromxoja <baxrom.aglamkhujaev@payme.uz>
2023-12-14 11:14:40 +0500

    fix: card's layout added

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { TransactionsComponent } from '../../core/components/transactions/transactions.component';
+import { SettingsComponent } from '../../core/components/settings/settings.component';
+import { CardComponent } from '../../core/components/card/card.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, TransactionsComponent, SettingsComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
