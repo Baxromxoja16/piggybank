648307379b3b12be9f3269e86f43d9d26742f1ed
Baxromxoja <baxrom.aglamkhujaev@payme.uz>
2023-12-15 10:22:36 +0500

    feat: added create account page

--- src/app/core/components/transactions/transactions.component.ts
+++ src/app/core/components/transactions/transactions.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CreateAccountComponent } from '../../../pages/components/create-account/create-account.component';
 
 @Component({
   selector: 'app-transactions',
   standalone: true,
-  imports: [],
+  imports: [CreateAccountComponent],
   templateUrl: './transactions.component.html',
   styleUrl: './transactions.component.scss'
 })
