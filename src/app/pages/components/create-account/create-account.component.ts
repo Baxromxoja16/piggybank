3471df9486e40d3e5102c1f504b6c6a5da28453a
Baxromxoja <baxrom.aglamkhujaev@payme.uz>
2023-12-17 11:16:16 +0500

    fix: added validation for create account form

--- src/app/pages/components/create-account/create-account.component.ts
+++ src/app/pages/components/create-account/create-account.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { UpDirective } from '../../../auth/directives/up.directive';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @Component({
   selector: 'app-create-account',
@@ -10,16 +13,20 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './create-account.component.scss'
 })
 export class CreateAccountComponent {
+  lettersReg = new RegExp('[A-Za-z]');
+  currencies = ['USD', 'EUR', 'GBP', 'JPY', 'UZS', 'RUB'];
+  selected = 'USD'
 
   createForm: FormGroup = new FormGroup({
-    title: new FormControl(null, Validators.required),
-    currency: new FormControl(null, Validators.required),
-    description: new FormControl(null, Validators.required),
+    title: new FormControl(null, [Validators.required, Validators.pattern(this.lettersReg)]),
+    currency: new FormControl(['USD']),
+    description: new FormControl(null, [Validators.maxLength(256)]),
   })
 
   constructor(){}
 
   onSubmit() {
-    console.log(this.createForm.valid);
+    console.log(this.createForm);
+    console.log(this.createForm.get('description')?.hasError('maxLength'));
   }
 }
