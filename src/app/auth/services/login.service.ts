f72645d897b1cf82460011d09e257f4d14001c49
Baxromxoja <baxrom.aglamkhujaev@payme.uz>
2024-01-03 16:31:16 +0500

    fix: login service session

--- src/app/auth/services/login.service.ts
+++ src/app/auth/services/login.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of, tap } from 'rxjs';
+import { Observable, catchError, of, tap } from 'rxjs';
 
 export interface UserLogin {
   email: string;
@@ -11,14 +11,29 @@ export interface UserLogin {
   providedIn: 'root',
 })
 export class LoginService {
-  test: Observable<UserLogin[]> = of([
-    { email: 'aglamof@mail.ru', password: '123asd' },
-  ]);
+  test: Observable<UserLogin> = of({ email: 'aglamof@mail.ru', password: '123asd' });
+  errorMessage: string = ''
 
   constructor(private http: HttpClient) {}
 
   login(data: UserLogin) {
     // return this.http.post('', data)
-    return this.test
+    return this.test.pipe(
+      tap((res) => this.setSession(res)),
+      catchError(this.handleError({}))
+    )
+  }
+
+  private setSession(res: any) {
+    const expiresIn = Date.now() + +res.expiresIn;
+    localStorage.setItem('myToken', res.token);
+    localStorage.setItem('expiresIn', String(expiresIn));
+  }
+
+  private handleError<T>(result: T) {
+    return (error: any): Observable<T> => {
+      this.errorMessage = error.error.message;
+      return of(result as T);
+    };
   }
 }
