9698ced0c3fab07c251bcbc0790a2aab40ec79ec
Baxromxoja <baxrom.aglamkhujaev@payme.uz>
2024-01-04 15:26:24 +0500

    feat: added account info

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { TransactionsComponent } from '../../core/components/transactions/transactions.component';
 import { SettingsComponent } from '../../core/components/settings/settings.component';
 import { CardComponent } from '../../core/components/card/card.component';
+import { AccountInfoComponent } from '../components/account-info/account-info.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CardComponent, TransactionsComponent, SettingsComponent],
+  imports: [CardComponent, TransactionsComponent, SettingsComponent, AccountInfoComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
