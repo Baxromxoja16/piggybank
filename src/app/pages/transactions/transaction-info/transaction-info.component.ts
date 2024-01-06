31bac02a55d5e4dab6c39485c8d67aab97f018fe
Baxromxoja <baxrom.aglamkhujaev@payme.uz>
2024-01-06 09:50:46 +0500

    feat: added transaction-info component

---
+++ src/app/pages/transactions/transaction-info/transaction-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './transaction-info.component.html',
+  styleUrl: './transaction-info.component.scss'
+})
+export class TransactionInfoComponent {
+
+}
