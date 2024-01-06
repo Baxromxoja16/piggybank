6d3446b6a09191e71434f5d5b5fde21bd757c31f
Baxromxoja <baxrom.aglamkhujaev@payme.uz>
2024-01-06 09:50:21 +0500

    feat: added transaction-create component

---
+++ src/app/pages/transactions/transaction-create/transaction-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './transaction-create.component.html',
+  styleUrl: './transaction-create.component.scss'
+})
+export class TransactionCreateComponent {
+
+}
