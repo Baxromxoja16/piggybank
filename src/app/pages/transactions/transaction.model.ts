4558e3bfb38258406ea92acf248ed758f07e0887
Baxromxoja <baxrom.aglamkhujaev@payme.uz>
2024-01-07 14:44:01 +0500

    feat: added transaction model

---
+++ src/app/pages/transactions/transaction.model.ts
@@ -0,0 +1,10 @@
+export interface ITransaction {
+  type: string;
+  title: string;
+  amount: number;
+  description?: string;
+  date_of_operation: Date;
+  categories: string[];
+  accountId: string;
+  payee?: string;
+}
