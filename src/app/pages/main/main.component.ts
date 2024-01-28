9b280118deaf675db035345d15640581517b5a96
baxromxoja16 <aglamof@mail.ru>
2024-01-28 22:45:29 +0500

    fix: navbar layout implemented

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -4,11 +4,12 @@ import { SettingsComponent } from '../settings/settings.component';
 import { CardComponent } from '../card/card.component';
 import { AccountInfoComponent } from '../card/account-info/account-info.component';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CardComponent, TransactionsComponent, SettingsComponent, AccountInfoComponent, RouterOutlet],
+  imports: [CardComponent, TransactionsComponent, SettingsComponent, AccountInfoComponent, RouterOutlet, NavbarComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
