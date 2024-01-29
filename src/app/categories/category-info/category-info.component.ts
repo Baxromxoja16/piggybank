11f33f7094b61299d0f8c88d7a52c1b30293c8ec
baxromxoja16 <aglamof@mail.ru>
2024-01-29 11:45:47 +0500

    feat: added search field component

--- src/app/categories/category-info/category-info.component.ts
+++ src/app/categories/category-info/category-info.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SearchFieldComponent } from '../../shared/components/search-field/search-field.component';
 
 @Component({
   selector: 'app-category-info',
   standalone: true,
-  imports: [],
+  imports: [SearchFieldComponent],
   templateUrl: './category-info.component.html',
   styleUrl: './category-info.component.scss'
 })
---
+++ src/app/shared/components/search-field/search-field.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-field',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-field.component.html',
+  styleUrl: './search-field.component.scss'
+})
+export class SearchFieldComponent {
+
+}
