65b6f7cee5f6f72eec44601bdccd64efeb202a70
baxromxoja16 <aglamof@mail.ru>
2024-01-29 21:18:55 +0500

    feat: added create category page and implemented

--- src/app/categories/create-category/create-category.component.ts
+++ src/app/categories/create-category/create-category.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { UpDirective } from '../../auth/directives/up.directive';
+import { CategoryService } from '../../pages/services/category.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-create-category',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatButtonToggleModule, UpDirective],
   templateUrl: './create-category.component.html',
   styleUrl: './create-category.component.scss'
 })
-export class CreateCategoryComponent {
+export class CreateCategoryComponent implements OnDestroy{
+  subscription: Subscription = new Subscription();
+
+  reg = new RegExp(/[a-zA-Z]/gi)
+  createForm: FormGroup = new FormGroup({
+    title: new FormControl(null, [Validators.required, Validators.pattern(this.reg)]),
+    type: new FormControl('income' || 'expense')
+  })
+
+  constructor(
+    private router: Router,
+    private categoryService: CategoryService,
+    private snackBar: MatSnackBar) {}
+
+  close() {
+    this.router.navigate(['/categories']);
+  }
+
+  onSubmit() {
+    if (this.createForm.valid) {
+      const createdCategory = this.categoryService.createCategory(this.createForm.value).subscribe(
+        () => {
+          this.snackBar.open("Category created!", "Close", {
+            duration: 4000
+          })
+          this.router.navigate(['/categories']);
+        }
+      );
+
+      this.subscription.add(createdCategory);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
+  }
 
 }
