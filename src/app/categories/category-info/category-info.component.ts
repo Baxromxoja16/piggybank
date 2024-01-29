1ab23101abce2e29b50cbd70dd9e1b9f96e49570
baxromxoja16 <aglamof@mail.ru>
2024-01-29 23:11:18 +0500

    feat: added category edit method

--- src/app/categories/category-info/category-info.component.ts
+++ src/app/categories/category-info/category-info.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, OnDestroy, WritableSignal, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -12,7 +13,14 @@ import { SearchFieldComponent } from '../../shared/components/search-field/searc
 @Component({
   selector: 'app-category-info',
   standalone: true,
-  imports: [SearchFieldComponent, MatProgressSpinnerModule, RouterLink, MatIconModule, CommonModule],
+  imports: [
+    SearchFieldComponent,
+    MatProgressSpinnerModule,
+    RouterLink,
+    MatIconModule,
+    CommonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './category-info.component.html',
   styleUrl: './category-info.component.scss'
 })
@@ -20,6 +28,14 @@ export class CategoryInfoComponent implements OnInit, OnDestroy {
   subscription: Subscription = new Subscription();
 
   categories: WritableSignal<ICategory[]> = this.categoryService.categories
+
+  editCategoryId!: string;
+
+  reg = new RegExp(/[a-zA-Z]/gi);
+  createForm: FormGroup = new FormGroup({
+    type: new FormControl('income' || 'expense'),
+    title: new FormControl('', [Validators.required, Validators.pattern(this.reg)])
+  })
 
   constructor(private categoryService: CategoryService, private snackBar: MatSnackBar) {}
 
@@ -38,6 +54,36 @@ export class CategoryInfoComponent implements OnInit, OnDestroy {
       }
     );
     this.subscription.add(deleteCategory);
+  }
+
+  editCategory(category: ICategory) {
+    this.createForm.get('type')?.setValue(category.type);
+    this.createForm.get('title')?.setValue(category.title);
+
+    if (!this.editCategoryId) this.editCategoryId = category._id;
+  }
+
+  onSubmit () {
+    if (this.createForm.valid) {
+      const newValue = this.createForm.value;
+
+      const editCategory = this.categoryService.updateCategory(this.editCategoryId, newValue).subscribe(
+        () => {
+          const message = 'Category updated!'
+          this.snackBar.open(message, 'Close', {
+            duration: 4000,
+          });
+
+          const newValueAddedDynamic = this.categories().filter(
+            (cat) => this.editCategoryId === cat._id ? cat.title = newValue.title : cat
+          );
+          this.categories.set(newValueAddedDynamic);
+
+          this.editCategoryId = '';
+        }
+      )
+      this.subscription.add(editCategory);
+    }
   }
 
   ngOnDestroy(): void {
