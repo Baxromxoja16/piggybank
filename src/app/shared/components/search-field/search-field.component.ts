7a0fe822b04d4ea3a9c3a868352d15ab97ebb32f
baxromxoja16 <aglamof@mail.ru>
2024-02-04 22:52:49 +0500

    feat: search component subscribing value changes

--- src/app/shared/components/search-field/search-field.component.ts
+++ src/app/shared/components/search-field/search-field.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-search-field',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './search-field.component.html',
   styleUrl: './search-field.component.scss'
 })
-export class SearchFieldComponent {
+export class SearchFieldComponent implements OnInit, OnDestroy {
+  subscription: Subscription = new Subscription();
 
+  createForm: FormGroup = new FormGroup({
+    search: new FormControl('')
+  });
+
+  ngOnInit(): void {
+    const valueChanges = this.createForm.valueChanges.subscribe((data) => {
+      console.log(data);
+    })
+
+    this.subscription.add(valueChanges);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
