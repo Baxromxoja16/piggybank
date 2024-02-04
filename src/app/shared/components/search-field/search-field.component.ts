1749f6e0fcfe45591345cba4d61b3f92e4063222
baxromxoja16 <aglamof@mail.ru>
2024-02-04 23:47:58 +0500

    feat: added filter for search

--- src/app/shared/components/search-field/search-field.component.ts
+++ src/app/shared/components/search-field/search-field.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, WritableSignal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { CategoryService, ICategory } from '../../../pages/services/category.service';
+import { TransactionService } from '../../../pages/services/transaction.service';
+import { ITransaction } from '../../../pages/transactions/transaction.model';
 
 @Component({
   selector: 'app-search-field',
@@ -10,15 +14,35 @@ import { Subscription } from 'rxjs';
   styleUrl: './search-field.component.scss'
 })
 export class SearchFieldComponent implements OnInit, OnDestroy {
+  @Input() dataCategoryOrTransaction!: WritableSignal<ICategory[] | ITransaction[]>;
+
   subscription: Subscription = new Subscription();
 
   createForm: FormGroup = new FormGroup({
     search: new FormControl('')
   });
 
+  constructor(
+    private router: Router,
+    private transactionService: TransactionService,
+    private categoryService: CategoryService,) {}
+
   ngOnInit(): void {
+    if (this.router.url === '/categories') {
+      this.getChangedValue<ICategory>(this.categoryService.categories)
+    } else if(this.router.url === '/transaction-info') {
+      this.getChangedValue<ITransaction>(this.transactionService.transactions)
+    }
+
+  }
+
+  private getChangedValue<T>(signals: WritableSignal<T[]>) {
     const valueChanges = this.createForm.valueChanges.subscribe((data) => {
-      console.log(data);
+      const searchPattern = new RegExp(data.search.trim(), 'i');
+      const filtered = signals().filter(
+        (item: any) => searchPattern.test(item.title)
+      );
+      console.log(filtered);
     })
 
     this.subscription.add(valueChanges);
