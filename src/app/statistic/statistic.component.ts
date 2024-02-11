bda71c270c00d99676b758b0f75ebf4157c20ce4
baxromxoja16 <aglamof@mail.ru>
2024-02-11 15:29:38 +0500

    feat: added basic layout for statistic

--- src/app/statistic/statistic.component.ts
+++ src/app/statistic/statistic.component.ts
@@ -2,14 +2,30 @@ import { Component } from '@angular/core';
 import { CardComponent} from '../pages/card/card.component'
 import { NavbarComponent } from '../shared/components/navbar/navbar.component';
 import {MatTabsModule} from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-statistic',
   standalone: true,
-  imports: [CardComponent, NavbarComponent, MatTabsModule],
+  imports: [
+    CardComponent,
+    NavbarComponent,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    ReactiveFormsModule,
+    MatNativeDateModule
+  ],
   templateUrl: './statistic.component.html',
   styleUrl: './statistic.component.scss'
 })
 export class StatisticComponent {
+  rangeDate = new FormGroup({
+    start: new FormControl(''),
+    end: new FormControl(''),
+  })
 
 }
