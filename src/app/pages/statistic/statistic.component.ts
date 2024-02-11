8c6e3a7361a0597395025cacae635f38982b9e11
baxromxoja16 <aglamof@mail.ru>
2024-02-11 11:41:26 +0500

    refactor: statistics folder changed

---
+++ src/app/pages/statistic/statistic.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-statistic',
+  standalone: true,
+  imports: [],
+  templateUrl: './statistic.component.html',
+  styleUrl: './statistic.component.scss'
+})
+export class StatisticComponent {
+
+}
