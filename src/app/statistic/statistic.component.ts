0d20001a14c01af4f9a577016e54096628e87878
baxromxoja16 <aglamof@mail.ru>
2024-02-11 14:27:27 +0500

    feat: added basic layout

--- src/app/statistic/statistic.component.ts
+++ src/app/statistic/statistic.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CardComponent} from '../pages/card/card.component'
+import { NavbarComponent } from '../shared/components/navbar/navbar.component';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-statistic',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, NavbarComponent, MatTabsModule],
   templateUrl: './statistic.component.html',
   styleUrl: './statistic.component.scss'
 })
