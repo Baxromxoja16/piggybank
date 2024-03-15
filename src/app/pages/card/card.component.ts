9b1ceaff99d0480ba340eac84693764e3c848777
baxromxoja16 <aglamof@mail.ru>
2024-03-15 15:32:17 +0500

    fix: added loader for account component

--- src/app/pages/card/card.component.ts
+++ src/app/pages/card/card.component.ts
@@ -19,15 +19,21 @@ export class CardComponent implements OnInit, OnDestroy {
   accounts: WritableSignal<Account[]> = this.accountService.accounts;
   subscription: Subscription = new Subscription();
   switchAccount: WritableSignal<Account> = this.accountService.switchAccountSig;
+  isLoading = false;
 
   constructor(private accountService: AccountService, private router: Router, private transactionService: TransactionService) {}
 
   ngOnInit(): void {
+    this.isLoading = true;
     const getAccount = this.accountService
       .getAccounts()
       .subscribe((data: Account[]) => {
+        this.isLoading = false;
         this.accounts.set(data)
         this.switchAccount = this.accountService.switchAccountSig
+      },
+      err => {
+        this.isLoading = false;
       });
 
     this.subscription.add(getAccount);
